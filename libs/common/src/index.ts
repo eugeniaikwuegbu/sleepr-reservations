093cd59a127575a7d12517b9e51d96f5a3557514
Eugenia Ikwuegbu <eugenia@zigah.co>
2024-09-05 13:15:14 +0100

    update index.ts

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -3,6 +3,5 @@ export * from './constants';
 export * from './database';
 export * from './decorators';
 export * from './dto';
-export * from './health';
 export * from './logger';
 export * from './models';
