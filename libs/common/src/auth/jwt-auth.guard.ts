0454f5c294d99a64e11071aaf5c7a0b6bbdfd736
Eugenia Ikwuegbu <eugenia@zigah.co>
2024-09-10 09:00:50 +0100

    update

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -11,10 +11,7 @@ export class JwtAuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
 
-    console.log('I got here');
     if (!jwt) return false;
-
-    console.log('I got here too');
 
     return this.authClient
       .send<UserDTO>('authenticate', {
